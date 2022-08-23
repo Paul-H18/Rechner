1ab33f43080803e17dcde161a81c7a22efd366ca
Paul <66725843+Paul-H18@users.noreply.github.com>
2022-08-23 19:30:43 +0200

    Start of Project

---
+++ src/de/paul/main/Main.java
@@ -0,0 +1,19 @@
+package de.paul.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        startUp();
+    }
+
+
+
+    public static void startUp() {
+        System.out.println("Calculator is staring...");
+
+
+
+        System.out.println("Calculator successfully started!");
+    }
+
+}
---
+++ src/de/paul/variables/Var.java
@@ -0,0 +1,4 @@
+package de.paul.variables;
+
+public class Var {
+}
